cfc9ca71e9dd60b9b23d91b69606e4ac8ae20f73
Markus Korporal <markus@korporal.de>
2021-09-13 11:09:12 +0200

    Create Handler.php

---
+++ Logger/Handler.php
@@ -0,0 +1,19 @@
+<?php
+namespace Iways\PayPalPlusLogger;
+
+use Monolog\Logger;
+
+class Handler extends \Magento\Framework\Logger\Handler\Base
+{
+    /**
+     * Logging level
+     * @var int
+     */
+    protected $loggerType = Logger::INFO;
+
+    /**
+     * File name
+     * @var string
+     */
+    protected $fileName = '/var/log/paypal-plus.log';
+}
